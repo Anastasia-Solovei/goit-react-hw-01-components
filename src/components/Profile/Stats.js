f43046139203da12f4fdf76983c99bb06842e7a5
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-07-21 13:10:41 +0300

    deletes ul

--- src/components/Profile/Stats.js
+++ src/components/Profile/Stats.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import s from './Stats.module.css';
+import PropTypes from 'prop-types';
 
 const Stats = ({ followers, views, likes }) => {
   return (
-    <ul className={s.list}>
+    <>
       <li className={s.item}>
         <span className={s.label}>Followers</span>
         <span className={s.quantity}>{followers}</span>
@@ -17,8 +18,14 @@ const Stats = ({ followers, views, likes }) => {
         <span className={s.label}>Likes</span>
         <span className={s.quantity}>{likes}</span>
       </li>
-    </ul>
+    </>
   );
+};
+
+Stats.propTypes = {
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
 };
 
 export default Stats;
