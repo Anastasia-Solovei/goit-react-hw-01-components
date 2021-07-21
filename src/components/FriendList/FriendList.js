ec31371ffb81d97d092af1d6651718304831e7ee
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-07-21 12:55:23 +0300

    fixes function map , component takes prop key

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-import s from './FriendList.module.css';
+import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem';
 
 const FriendList = ({ friends }) => {
-  if (friends.length === 0) {
-    return null;
-  }
+  return friends.map(friend => (
+    <FriendListItem key={friend.id} friend={friend} />
+  ));
+};
 
-  return <ul className={s.list}>{friends.map(FriendListItem)}</ul>;
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
 };
 
 export default FriendList;
