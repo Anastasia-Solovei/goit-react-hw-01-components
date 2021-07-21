bf62e06e9cd16cc7acabda6d5c34b02ea1564951
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-07-21 13:07:30 +0300

    remove other components from this file and connects imports with them / task 2

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,53 +2,23 @@ import React from 'react';
 
 import s from './Statistics.module.css';
 import PropTypes from 'prop-types';
-import randomBgColor from './random-bg-color';
-
-const StatListItem = ({ id, label, percentage }) => {
-  return (
-    <li
-      className={s.item}
-      key={id}
-      style={{ backgroundColor: randomBgColor() }}
-    >
-      <span className={s.label}>{label}</span>
-      <span className={s.percentage}>{percentage}%</span>
-    </li>
-  );
-};
-
-const StatList = ({ items }) => {
-  if (items.length === 0) {
-    return null;
-  }
-
-  return <ul className={s.list}>{items.map(StatListItem)}</ul>;
-};
+import StatList from './StatList';
 
 const Statistics = ({ title, stats }) => {
   return (
     <section className={s.container}>
       {title && <h2 className={s.title}>{title}</h2>}
 
-      <StatList items={stats} />
+      <ul className={s.list}>
+        <StatList items={stats} />
+      </ul>
     </section>
   );
 };
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  stats: PropTypes.arrayOf(PropTypes.shape),
-};
-
-StatList.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.string })),
-};
-
-StatListItem.propTypes = {
-  item: PropTypes.shape({
-    label: PropTypes.string.isRequired,
-    percentage: PropTypes.number.isRequired,
-  }),
+  stats: PropTypes.arrayOf(PropTypes.shape).isRequired,
 };
 
 export default Statistics;
